1eeaf44c627565039a3f8709cbc0687399eb2985
Ryan Lightbody <Ryan Lightbody>
2016-07-21 16:12:10 -0400

    Finished?

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+	var ipAddr = req.headers["x-forwarded-for"];
+	if (ipAddr){
+		var list = ipAddr.split(",");
+		ipAddr = list[list.length-1];
+	} else {
+		ipAddr = req.connection.remoteAddress;
+	}
 	var headerObj = {
-		"ipaddress": req.ip,
+		"ipaddress": ipAddr,
 		"language": req.headers["accept-language"].split(",")[0],
 		"software": req.headers["user-agent"].match(/\((.*?)\)/)[1]
 	}
